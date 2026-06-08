6fcc1a7756c98fd0fc1f1cf948ff051c8e8d0f3e
altno54321-dotcom <altno.54321@gmail.com>
2026-06-08 20:23:04 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// This makes the app installable
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Just passes requests through normally
-});
